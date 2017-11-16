a51137a41b31dff4d770de49dac64aa18e95d9fc
Vitaliy <theyamshikov@yandex.ru>
2017-11-16 16:15:30 +0600

    get header from header

--- src/helpers/Api.php
+++ src/helpers/Api.php
@@ -9,10 +9,13 @@ class Api
 	{
 		$version = null;
 		if(APP == API && !empty($_SERVER['REQUEST_URI'])) {
-			$url = $_SERVER['REQUEST_URI'];
-			if (preg_match('#/v([0-9]+)(/|$)#i', $url, $matches)) {
-				$version = $matches[1];
-				//$_SERVER['REQUEST_URI'] = str_replace('/v' . $version, '', $_SERVER['REQUEST_URI']);
+			$version = self::getApiVersionFromUrl();
+			if(empty($version)) {
+				$version = self::getApiVersionFromHeader();
+			}
+			if(empty($version)) {
+				header('HTTP/1.1 400 Bad Request', true, 400);
+				exit('No API version specified');
 			}
 		}
 		return $version;
@@ -34,6 +37,28 @@ class Api
 			header("HTTP/1.0 200 Ok");
 			exit();
 		}
+	}
+	
+	private static function getApiVersionFromUrl()
+	{
+		$version = null;
+		$url = $_SERVER['REQUEST_URI'];
+		if (preg_match('#/v([0-9]+)(/|$)#i', $url, $matches)) {
+			$version = $matches[1];
+			//$_SERVER['REQUEST_URI'] = str_replace('/v' . $version, '', $_SERVER['REQUEST_URI']);
+		}
+		return $version;
+	}
+	
+	private static function getApiVersionFromHeader()
+	{
+		$version = null;
+		$allHeaders = getallheaders();
+		if(!empty($allHeaders['Version'])) {
+			$version = $allHeaders['Version'];
+			$_SERVER['REQUEST_URI'] = '/v' . $version . $_SERVER['REQUEST_URI'];
+		}
+		return $version;
 	}
 	
 	private static function initCorsConfig($cors)
