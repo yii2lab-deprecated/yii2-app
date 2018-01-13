3076c3a7aa64f22657dc58b1fb476caf62167655
Vitaliy <theyamshikov@yandex.ru>
2018-01-13 21:36:37 +0600

    fix

--- src/domain/filters/config/FixValidationKeyInTest.php
+++ src/domain/filters/config/FixValidationKeyInTest.php
@@ -8,7 +8,7 @@ use yii2lab\designPattern\filter\interfaces\FilterInterface;
 class FixValidationKeyInTest extends BaseObject implements FilterInterface {
 
 	public function run($config) {
-		if(YII_ENV_TEST && empty($config['components']['request']['cookieValidationKey'])) {
+		if(APP != CONSOLE && empty($config['components']['request']['cookieValidationKey'])) {
 			$config['components']['request']['cookieValidationKey'] = 'testValidationKey'; // костыль, чтоб не выдавало ошибку при тестах в common
 		}
 		return $config;
