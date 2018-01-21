8b9d1238146b6cf5a6197ffd6e3824bee8a87e54
Vitaliy <theyamshikov@yandex.ru>
2018-01-21 20:33:05 +0600

    update test

--- tests/store/definitionEnv.php
+++ tests/store/definitionEnv.php
@@ -7,7 +7,7 @@ return [
 			'class' => 'yii2lab\\app\\domain\\filters\\env\\LoadConfig',
 			'paths' => [
 				'vendor/yii2lab/yii2-app/tests/store/app/common/config',
-				'vendor/yii2lab/yii2-app/src/domain/config',
+				'vendor/yii2lab/yii2-app/src/application/common/config',
 			],
 		],
 		'yii2lab\\app\\domain\\filters\\env\\YiiEnv',
