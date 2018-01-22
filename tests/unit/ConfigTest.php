e86d85113a1af098875e4cec1ea8c591f8fc8382
Vitaliy <theyamshikov@yandex.ru>
2018-01-22 13:28:04 +0600

    refacto

--- tests/unit/ConfigTest.php
+++ tests/unit/ConfigTest.php
@@ -1,30 +1,39 @@
 <?php
+
 namespace api\tests\unit\helpers;
 
 use Codeception\Test\Unit;
 use yii\helpers\ArrayHelper;
 use yii2lab\app\domain\helpers\Config;
 use yii2lab\app\domain\helpers\Env;
-use yii2lab\store\Store;
+use UnitTester;
+use yii2lab\test\helpers\DataHelper;
 
+/**
+ * Class ConfigTest
+ *
+ * @package api\tests\unit\helpers
+ *
+ * @property UnitTester $tester
+ */
 class ConfigTest extends Unit
 {
 	
-	const DATA_PATH_ALIAS = '@vendor/yii2lab/yii2-app/tests/store/';
+	const PACKAGE = 'yii2lab/yii2-app';
 	
 	public function testLoadConfig()
 	{
-		$env = $this->loadData('app/common/config/env.php');
-		$envLocal = $this->loadData('app/common/config/env-local.php');
+		$env = DataHelper::load(self::PACKAGE, 'store/app/common/config/env.php');
+		$envLocal = DataHelper::load(self::PACKAGE, 'store/app/common/config/env-local.php');
 		$env = ArrayHelper::merge($env, $envLocal);
 		$config = Config::load($env['config']);
-		$configExpect = $this->loadData('resultConfig.php', $config);
+		$configExpect = DataHelper::load(self::PACKAGE, 'store/resultConfig.php', $config);
 		expect($configExpect)->equals($config);
 	}
 	
 	public function testEnvGetDefinition()
 	{
-		$definition = $this->loadData('definitionEnv.php');
+		$definition = DataHelper::load(self::PACKAGE, 'store/definitionEnv.php');
 		$definitionGenerated = Env::getDefinition('vendor/yii2lab/yii2-app/tests/store/app');
 		expect($definition)->equals($definitionGenerated);
 		
@@ -34,25 +43,10 @@ class ConfigTest extends Unit
 	
 	public function testLoadEnv()
 	{
-		$definition = $this->loadData('definitionEnv.php');
+		$definition = DataHelper::load(self::PACKAGE, 'store/definitionEnv.php');
 		$config = Env::load($definition);
-		$configExpect = $this->loadData('resultEnv.php', $config);
+		$configExpect = DataHelper::load(self::PACKAGE, 'store/resultEnv.php', $config);
 		expect($configExpect)->equals($config);
-	}
-	
-	private function loadData($filename, $defaultData = null) {
-		$store = new Store('php');
-		$configExpect = $store->load(self::DATA_PATH_ALIAS . $filename);
-		if(empty($configExpect)) {
-			$this->saveData($filename, $defaultData);
-			return $defaultData;
-		}
-		return $configExpect;
-	}
-	
-	private function saveData($filename, $data) {
-		$store = new Store('php');
-		$store->save(self::DATA_PATH_ALIAS . $filename, $data);
 	}
 	
 }
