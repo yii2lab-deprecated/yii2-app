6380d1af0b0670c6c15c237851e4d4a3dadf4754
Vitaliy <theyamshikov@yandex.ru>
2018-03-30 15:28:36 +0600

    make docblock in domain

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -4,6 +4,17 @@ namespace yii2lab\app\domain;
 
 use yii2lab\domain\enums\Driver;
 
+/**
+ * Class Domain
+ *
+ * @package yii2lab\app\domain
+ *
+ * @property \yii2lab\app\domain\services\ModeService $mode
+ * @property \yii2lab\app\domain\services\UrlService $url
+ * @property \yii2lab\app\domain\services\RemoteService $remote
+ * @property \yii2lab\app\domain\services\ConnectionService $connection
+ * @property \yii2lab\app\domain\services\CookieService $cookie
+ */
 class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
