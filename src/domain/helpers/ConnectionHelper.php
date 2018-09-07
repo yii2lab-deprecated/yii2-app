d593f22dfb8037e14a3303b86d239149affbe169
Vitaliy <theyamshikov@yandex.ru>
2018-09-07 18:39:55 +0600

    refacto

--- src/domain/helpers/ConnectionHelper.php
+++ src/domain/helpers/ConnectionHelper.php
@@ -2,41 +2,15 @@
 
 namespace yii2lab\app\domain\helpers;
 
-use Yii;
-use yii\db\Exception;
-use yii\db\Connection;
-use yii2lab\db\domain\helpers\DbHelper;
-use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
-use yii2lab\domain\helpers\ErrorCollection;
-
-class ConnectionHelper {
+/**
+ * Class ConnectionHelper
+ *
+ * @package yii2lab\app\domain\helpers
+ *
+ * @deprecated
+ */
+class ConnectionHelper extends \yii2lab\db\domain\helpers\ConnectionHelper {
 	
-	public static function test(array $config) {
-		//$config = DbHelper::normalizeConfig($config);
-		//$config = DbHelper::schemaMap($config);
-		$connection = Yii::createObject(Connection::class);
-		$connection->dsn = $config['dsn'];
-		$connection->username = $config['username'];
-		$connection->password = $config['password'];
-		try {
-			$connection->open();
-		} catch(Exception $e) {
-			$message = $e->getMessage();
-			$message = trim($message);
-			$error = new ErrorCollection();
-			$previous2 = $e->getPrevious()->getPrevious();
-			if($previous2 != null && preg_match('~getaddrinfo failed~', $previous2->getMessage())) {
-				$error->add('host', Yii::t('app/connection', 'bad_host'));
-			}
-			if(preg_match('~Unknown database~', $message)) {
-				$error->add('dbname', Yii::t('app/connection', 'bad_dbname'));
-			}
-			if(preg_match('~Access denied for user~', $message)) {
-				$error->add('username', Yii::t('app/connection', 'bad_username'));
-				$error->add('password', Yii::t('app/connection', 'bad_password'));
-			}
-			throw new UnprocessableEntityHttpException($error);
-		}
-	}
+
 	
 }
