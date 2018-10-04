59b98445ff1bfbc988ca474ef8e9f04f8f78ccc1
Vitaliy <theyamshikov@yandex.ru>
2018-10-04 22:40:42 +0600

    clean

--- src/application/common/config/env-base.php
+++ src/application/common/config/env-base.php
@@ -4,51 +4,53 @@ use yii2lab\app\domain\commands\RunBootstrap;
 use yii2lab\app\domain\enums\YiiEnvEnum;
 use yii2lab\app\domain\filters\config\LoadConfig;
 
+$isTest = YII_ENV == YiiEnvEnum::TEST;
+
 return [
-    'app' => [
-        'commands' => [
-            [
-                'class' => RunBootstrap::class,
-                'app' => COMMON,
-            ],
-            [
-                'class' => RunBootstrap::class,
-                'app' => APP,
-            ],
-        ],
-    ],
-    'config' => [
-        'filters' => [
-            [
-                'class' => LoadConfig::class,
-                'app' => COMMON,
-                'name' => 'main',
-                'withLocal' => true,
-                'isEnabled' => YII_ENV != YiiEnvEnum::TEST,
-            ],
-            [
-                'class' => LoadConfig::class,
-                'app' => APP,
-                'name' => 'main',
-                'withLocal' => true,
-                'isEnabled' => YII_ENV != YiiEnvEnum::TEST,
-            ],
-
-            [
-                'class' => LoadConfig::class,
-                'app' => COMMON,
-                'name' => 'test-local',
-                'withLocal' => false,
-                'isEnabled' => YII_ENV == YiiEnvEnum::TEST,
-            ],
-
-            [
-                'class' => LoadConfig::class,
-                'app' => APP,
-                'name' => 'test-local',
-                'withLocal' => false,
-                'isEnabled' => YII_ENV == YiiEnvEnum::TEST,
-            ],
-        ],
-    ],
+	'app' => [
+		'commands' => [
+			[
+				'class' => RunBootstrap::class,
+				'app' => COMMON,
+			],
+			[
+				'class' => RunBootstrap::class,
+				'app' => APP,
+			],
+		],
+	],
+	'config' => [
+		'filters' => [
+			[
+				'class' => LoadConfig::class,
+				'app' => COMMON,
+				'name' => 'main',
+				'withLocal' => true,
+				'isEnabled' => !$isTest,
+			],
+			[
+				'class' => LoadConfig::class,
+				'app' => APP,
+				'name' => 'main',
+				'withLocal' => true,
+				'isEnabled' => !$isTest,
+			],
+			
+			[
+				'class' => LoadConfig::class,
+				'app' => COMMON,
+				'name' => 'test-local',
+				'withLocal' => false,
+				'isEnabled' => $isTest,
+			],
+			
+			[
+				'class' => LoadConfig::class,
+				'app' => APP,
+				'name' => 'test-local',
+				'withLocal' => false,
+				'isEnabled' => $isTest,
+			],
+		],
+	],
 ];
