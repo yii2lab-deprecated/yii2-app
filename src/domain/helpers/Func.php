0e89694ab65fa9877bec81dfe19adde480af8748
Vitaliy <theyamshikov@yandex.ru>
2018-10-04 20:35:50 +0600

    make methods

--- src/domain/helpers/Func.php
+++ src/domain/helpers/Func.php
@@ -3,6 +3,7 @@
 use yii\helpers\ArrayHelper;
 use yii2lab\app\domain\helpers\Config;
 use yii2lab\app\domain\helpers\Env;
+use yii2lab\helpers\Debug;
 
 function config($name, $default = null) {
 	$value = Config::get($name);
@@ -34,4 +35,35 @@ function param($name, $default = null) {
 		$value = $default;
 	}
 	return $value;
+}
+
+function prr($val, $exit = false, $forceToArray = false) {
+	Debug::prr($val, $exit, $forceToArray);
+}
+
+function mb_ucfirst($word) {
+	return mb_strtoupper(mb_substr($word, 0, 1, 'UTF-8'), 'UTF-8') . mb_substr(mb_convert_case($word, MB_CASE_LOWER, 'UTF-8'), 1, mb_strlen($word), 'UTF-8');
+}
+
+function sortByLen($a, $b) {
+	if(strlen($a) < strlen($b)) {
+		return 1;
+	} elseif(strlen($a) == strlen($b)) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+/**
+ * @param       $category
+ * @param       $message
+ * @param array $params
+ * @param null  $language
+ *
+ * @return string
+ * @deprecated
+ */
+function t($category, $message, $params = [], $language = null) {
+	return \Yii::t($category, $message, $params, $language);
 }
