63b6fb59004dc0ad1999d2b990b071f79e5a5291
Vitaliy <theyamshikov@yandex.ru>
2018-11-08 17:41:29 +0600

    make rest data

---
+++ src/domain/helpers/generator/applicationFiles/common/fixtures/data/rest.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	2 => [
+		'id' => '2',
+		'tag' => '4b1a5f75',
+		'module_id' => 'rest-v1',
+		'request' => 'a:14:{s:6:"method";s:3:"get";s:8:"endpoint";s:4:"auth";s:3:"tab";s:1:"1";s:9:"queryKeys";a:0:{}s:11:"queryValues";a:0:{}s:12:"queryActives";a:0:{}s:8:"bodyKeys";a:0:{}s:10:"bodyValues";a:0:{}s:11:"bodyActives";a:0:{}s:10:"headerKeys";a:0:{}s:12:"headerValues";a:0:{}s:13:"headerActives";a:0:{}s:11:"description";s:0:"";s:13:"authorization";s:11:"77771111111";}',
+		'response' => 'Array',
+		'method' => 'get',
+		'endpoint' => 'auth',
+		'description' => null,
+		'status' => null,
+		'stored_at' => null,
+		'favorited_at' => '1541675925',
+	],
+	4 => [
+		'id' => '4',
+		'tag' => '353d11e1',
+		'module_id' => 'rest-v1',
+		'request' => 'a:14:{s:6:"method";s:4:"post";s:8:"endpoint";s:4:"auth";s:3:"tab";s:1:"2";s:9:"queryKeys";a:0:{}s:11:"queryValues";a:0:{}s:12:"queryActives";a:0:{}s:8:"bodyKeys";a:2:{i:0;s:5:"login";i:1;s:8:"password";}s:10:"bodyValues";a:2:{i:0;s:11:"77771111111";i:1;s:9:"Wwwqqq111";}s:11:"bodyActives";a:2:{i:0;s:1:"1";i:1;s:1:"1";}s:10:"headerKeys";a:0:{}s:12:"headerValues";a:0:{}s:13:"headerActives";a:0:{}s:11:"description";s:0:"";s:13:"authorization";s:11:"77771111111";}',
+		'response' => 'Array',
+		'method' => 'post',
+		'endpoint' => 'auth',
+		'description' => null,
+		'status' => null,
+		'stored_at' => null,
+		'favorited_at' => '1541675956',
+	],
+];
